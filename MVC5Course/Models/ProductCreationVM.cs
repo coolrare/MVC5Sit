10b3f25c8fe3a0ba664e21fffcebc2f685eb2833
Will <doggy.huang@gmail.com>
2017-11-10 17:55:10 +0800

    24 練習如何抓到 SaveChange() 時的實體驗證錯誤，並修改 ViewModel 的模型驗證邏輯

--- MVC5Course/Models/ProductCreationVM.cs
+++ MVC5Course/Models/ProductCreationVM.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace MVC5Course.Models
 {
-    public class ProductCreationVM
+    public class ProductCreationVM : IValidatableObject
     {
         public int ProductId { get; set; }
 
@@ -18,5 +18,15 @@ namespace MVC5Course.Models
         public Nullable<decimal> Price { get; set; }
 
         public int OrderLineCount { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!this.ProductName.Contains("台灣"))
+            {
+                yield return new ValidationResult(
+                    "台灣只能有一個",
+                    new string[] { "ProductName" });
+            }
+        }
     }
 }
