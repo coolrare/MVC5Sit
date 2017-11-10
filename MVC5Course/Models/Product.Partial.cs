33e9437d3e9533419d575a7b8bb16002a6c553ca
Will <doggy.huang@gmail.com>
2017-11-10 13:21:24 +0800

    19 練習在資料模型實作 IValidatableObject 介面 ( 模型驗證 )

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -4,18 +4,27 @@ namespace MVC5Course.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ProductMetaData))]
-    public partial class Product
+    public partial class Product : IValidatableObject
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.ProductName.Contains("台灣") && this.Stock <= 0)
+            {
+                yield return new ValidationResult(
+                    "台灣只能有一個",
+                    new string[] { "ProductName", "Stock" });
+            }
+        }
     }
-    
+
     public partial class ProductMetaData
     {
         public int ProductId { get; set; }
-        
+
         [Required]
-        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [StringLength(80, ErrorMessage = "欄位長度不得大於 80 個字元")]
         [ValidateTaiwanAddress]
         public string ProductName { get; set; }
         [Required]
@@ -24,7 +33,7 @@ namespace MVC5Course.Models
         public Nullable<bool> Active { get; set; }
         [Required]
         public Nullable<decimal> Stock { get; set; }
-    
+
         public virtual ICollection<OrderLine> OrderLine { get; set; }
     }
 }
